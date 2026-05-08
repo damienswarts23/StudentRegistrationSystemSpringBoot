632aad79e0c202f137d559c7f40d963f92d0cbe6
Damien Swarts <222868791@mycput.ac.za>
2026-05-08 16:34:43 +0200

    i impemented all the crud methods in the student services class and used the tools that the framework allows me to

--- src/main/java/za/ac/mycput/studentregistrationsystemspringboot/service/StudentService.java
+++ src/main/java/za/ac/mycput/studentregistrationsystemspringboot/service/StudentService.java
@@ -1,4 +1,54 @@
 package za.ac.mycput.studentregistrationsystemspringboot.service;
 
-public class StudentService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import za.ac.mycput.studentregistrationsystemspringboot.domain.Student;
+import za.ac.mycput.studentregistrationsystemspringboot.repository.StudentRepository;
+
+import java.util.List;
+
+/*
+StudentService.java
+Service class for managing Student objects.
+Implements the Singleton pattern and provides CRUD operations.
+
+Author: Damien Swarts (222868791)
+Date: 08 May 2026
+*/
+@Service
+public class StudentService implements IStudentService{
+
+    @Autowired
+    private StudentRepository repository;
+
+    @Override
+    public Student create(Student student){
+        if(student == null){
+            return null;
+        }
+        return repository.save(student);
+    }
+
+    @Override
+    public Student read(String id) {
+        return repository.findById(id).orElse(null);
+    }
+
+    @Override
+    public List<Student> getAll() {
+        return repository.findAll();
+    }
+
+    @Override
+    public Student update(Student student) {
+        if (student == null){
+            return null;
+        }
+        return repository.save(student);
+    }
+
+    @Override
+    public void delete(String id) {
+        repository.deleteById(id);
+    }
 }
