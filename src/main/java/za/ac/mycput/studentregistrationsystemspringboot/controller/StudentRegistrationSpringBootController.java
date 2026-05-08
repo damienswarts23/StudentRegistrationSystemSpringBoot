b4c83ee6dc9d14dc69b1e3de835dc6c3a88e007a
Damien Swarts <222868791@mycput.ac.za>
2026-05-08 17:24:50 +0200

    I added the different functions for the different services in springboot to a student class, next i will test it using postman

--- src/main/java/za/ac/mycput/studentregistrationsystemspringboot/controller/StudentRegistrationSpringBootController.java
+++ src/main/java/za/ac/mycput/studentregistrationsystemspringboot/controller/StudentRegistrationSpringBootController.java
@@ -1,12 +1,56 @@
 package za.ac.mycput.studentregistrationsystemspringboot.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+/*
+StudentRegistrationSpringBootController.java
+This controller class is made to manage the project and call functions etc.
+
+Author: Damien Swarts (222868791)
+Date: 08 May 2026
+*/
+
+import org.springframework.web.bind.annotation.*;
+import za.ac.mycput.studentregistrationsystemspringboot.domain.Student;
+import za.ac.mycput.studentregistrationsystemspringboot.service.StudentService;
+
+import java.util.List;
 
 @RestController
 public class StudentRegistrationSpringBootController {
-    @GetMapping
-    public String homeStudentRegistrationSpringBoot(){
-        return "This springboot Application is working";
+
+    //Call this Constructor independincy
+    private final StudentService service;
+    public StudentRegistrationSpringBootController(StudentService service){
+        this.service = service;
     }
+
+    //Post mapping is used to create orders
+    @PostMapping
+    public Student create(@RequestBody Student student){
+        return service.create(student);
+    }
+
+    //This allows us to get the object by its id
+    @GetMapping("/{id}")
+    public Student read(@PathVariable String id){
+        return service.read(id);
+    }
+
+    //This allows us to get all students
+    @GetMapping
+    public List<Student> getAll(){
+        return service.getAll();
+    }
+
+    //Putmapping allows us to update a record
+    @PutMapping
+    public Student update(@RequestBody Student student){
+        return service.update(student);
+    }
+
+    //This allows us to delete a object
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable String id){
+        service.delete(id);
+    }
+
 }
