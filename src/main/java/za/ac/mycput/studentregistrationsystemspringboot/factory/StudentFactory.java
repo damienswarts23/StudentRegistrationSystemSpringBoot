91b079849d449b657e06186b377dce6da9912615
Damien Swarts <222868791@mycput.ac.za>
2026-05-08 15:55:12 +0200

    Created a student factory class which is used for creating student objects

--- src/main/java/za/ac/mycput/studentregistrationsystemspringboot/factory/StudentFactory.java
+++ src/main/java/za/ac/mycput/studentregistrationsystemspringboot/factory/StudentFactory.java
@@ -1,4 +1,26 @@
 package za.ac.mycput.studentregistrationsystemspringboot.factory;
 
+/*
+StudentFactory.java
+Factory class used to create Student objects.
+Utilizes the Builder Pattern for flexible and readable object creation.
+
+Author: Damien Swarts (222868791)
+Date: 08 May 2026
+*/
+import za.ac.mycput.studentregistrationsystemspringboot.domain.*;
+
 public class StudentFactory {
+    public static Student createStudent(int studentID, String firstName, String lastName, Address address,
+                                        ContactDetails contactDetails, Gender gender, Race race){
+        return new Student.Builder()
+                .setStudentID(studentID)
+                .setFirstName(firstName)
+                .setLastName(lastName)
+                .setAddress(address)
+                .setContactDetails(contactDetails)
+                .setGender(gender)
+                .setRace(race)
+                .build();
+    }
 }
