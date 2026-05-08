1574edbadcb5a4c6c61e1c2c7e3a6c15f451a68d
Damien Swarts <222868791@mycput.ac.za>
2026-05-08 15:12:07 +0200

    added the race options for the enum class so when the student or lecturer can choose their race

--- src/main/java/za/ac/mycput/studentregistrationsystemspringboot/domain/Race.java
+++ src/main/java/za/ac/mycput/studentregistrationsystemspringboot/domain/Race.java
@@ -1,4 +1,16 @@
 package za.ac.mycput.studentregistrationsystemspringboot.domain;
 
+/*
+Race.java
+Enumeration representing race categories for a person.
+
+Author: Damien Swarts (222868791)
+Date: 08 May 2026
+*/
 public enum Race {
+    AFRICAN,
+    COLOURED,
+    INDIAN,
+    WHITE,
+    OTHER
 }
