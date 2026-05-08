e36233a366337d642c15ea8092113936fef4f7de
Damien Swarts <222868791@mycput.ac.za>
2026-05-08 16:37:53 +0200

    Added a helper class because why not

---
+++ src/main/java/za/ac/mycput/studentregistrationsystemspringboot/util/Helper.java
@@ -0,0 +1,15 @@
+package za.ac.mycput.studentregistrationsystemspringboot.util;
+
+/*
+Helper.java
+The helper class help me with validation where needed.
+
+Author: Damien Swarts (222868791)
+Date: 08 May 2026
+*/
+
+public class Helper {
+    public static boolean isNullOrEmpty(String str){
+        return str == null || str.isEmpty();
+    }
+}
