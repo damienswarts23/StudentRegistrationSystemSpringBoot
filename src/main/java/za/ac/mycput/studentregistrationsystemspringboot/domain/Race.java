e81e92327c3177cb0082648a6a6b5b6cdaa7314c
Damien Swarts <222868791@mycput.ac.za>
2026-05-08 17:03:20 +0200

    Had to make the Race class embeddable because it is being used by the person abstract class

--- src/main/java/za/ac/mycput/studentregistrationsystemspringboot/domain/Race.java
+++ src/main/java/za/ac/mycput/studentregistrationsystemspringboot/domain/Race.java
@@ -1,5 +1,7 @@
 package za.ac.mycput.studentregistrationsystemspringboot.domain;
 
+import jakarta.persistence.Embeddable;
+
 /*
 Race.java
 Enumeration representing race categories for a person.
@@ -7,6 +9,7 @@ Enumeration representing race categories for a person.
 Author: Damien Swarts (222868791)
 Date: 08 May 2026
 */
+@Embeddable
 public enum Race {
     AFRICAN,
     COLOURED,
