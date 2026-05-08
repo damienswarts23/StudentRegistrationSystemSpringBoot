8cb195578b12c077907f0a6d49e3f5046bc5eed6
Damien Swarts <222868791@mycput.ac.za>
2026-05-08 17:07:41 +0200

    Had to add the Entity tag and the table tag to the class and identify its id variable. also had to add the default constructor to the class since the super class also has the constructor

--- src/main/java/za/ac/mycput/studentregistrationsystemspringboot/domain/Student.java
+++ src/main/java/za/ac/mycput/studentregistrationsystemspringboot/domain/Student.java
@@ -1,5 +1,9 @@
 package za.ac.mycput.studentregistrationsystemspringboot.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
 /*
 Student.java
 Student entity representing a student.
@@ -9,11 +13,18 @@ Inherits common attributes from the Person class.
 Author: Damien Swarts (222868791)
 Date: 08 May 2026
 */
+@Entity
+@Table(name = "Student")
+
 public class Student extends Person{
+    @Id
     private String studentID;
     private String firstName;
     private String lastName;
 
+    protected Student() {
+        super();
+    }
     private Student(Builder builder) {
         super(builder.address, builder.contactDetails, builder.gender, builder.race);
         this.studentID = builder.studentID;
