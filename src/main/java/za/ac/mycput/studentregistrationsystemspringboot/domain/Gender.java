b444bddfd68b113bf79085f46dc6b6beb023406c
Damien Swarts <222868791@mycput.ac.za>
2026-05-08 18:13:35 +0200

    enum classes should not have embeddable

--- src/main/java/za/ac/mycput/studentregistrationsystemspringboot/domain/Gender.java
+++ src/main/java/za/ac/mycput/studentregistrationsystemspringboot/domain/Gender.java
@@ -1,6 +1,5 @@
 package za.ac.mycput.studentregistrationsystemspringboot.domain;
 
-import jakarta.persistence.Embeddable;
 
 /*
 Gender.java
@@ -9,7 +8,7 @@ Enumeration representing gender categories for a person.
 Author: Damien Swarts (222868791)
 Date: 08 May 2026
 */
-@Embeddable
+
 public enum Gender {
     MALE,
     FEMALE,
--- src/main/java/za/ac/mycput/studentregistrationsystemspringboot/domain/Race.java
+++ src/main/java/za/ac/mycput/studentregistrationsystemspringboot/domain/Race.java
@@ -1,6 +1,6 @@
 package za.ac.mycput.studentregistrationsystemspringboot.domain;
 
-import jakarta.persistence.Embeddable;
+
 
 /*
 Race.java
@@ -9,7 +9,6 @@ Enumeration representing race categories for a person.
 Author: Damien Swarts (222868791)
 Date: 08 May 2026
 */
-@Embeddable
 public enum Race {
     AFRICAN,
     COLOURED,
