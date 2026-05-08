f78a773102df0fc22167beb4a44979619afd5b2d
Damien Swarts <222868791@mycput.ac.za>
2026-05-08 15:06:54 +0200

    Added the phone number and the email of the person

--- src/main/java/za/ac/mycput/studentregistrationsystemspringboot/domain/ContactDetails.java
+++ src/main/java/za/ac/mycput/studentregistrationsystemspringboot/domain/ContactDetails.java
@@ -1,4 +1,25 @@
 package za.ac.mycput.studentregistrationsystemspringboot.domain;
 
+/*
+ContactDetails.java
+ContactDetails class representing contact information
+(email and phone number) for a person.
+
+Author: Damien Swarts (222868791)
+Date: 08 May 2026
+*/
+
 public class ContactDetails {
+    private String email;
+    private String phone;
+
+    public ContactDetails(String email, String phone) {
+        this.email = email;
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return "Email: " + email + ", Phone: " + phone;
+    }
 }
