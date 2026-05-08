a1d231c0000b5c6480fc967c149e68cdf440396c
Damien Swarts <222868791@mycput.ac.za>
2026-05-08 17:03:08 +0200

    Had to make the Gender class embeddable because it is being used by the person abstract class

--- src/main/java/za/ac/mycput/studentregistrationsystemspringboot/domain/Gender.java
+++ src/main/java/za/ac/mycput/studentregistrationsystemspringboot/domain/Gender.java
@@ -1,5 +1,7 @@
 package za.ac.mycput.studentregistrationsystemspringboot.domain;
 
+import jakarta.persistence.Embeddable;
+
 /*
 Gender.java
 Enumeration representing gender categories for a person.
@@ -7,6 +9,7 @@ Enumeration representing gender categories for a person.
 Author: Damien Swarts (222868791)
 Date: 08 May 2026
 */
+@Embeddable
 public enum Gender {
     MALE,
     FEMALE,
