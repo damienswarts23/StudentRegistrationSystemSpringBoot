f1767cb3ddf51a0d5463d4dd09f6bbda681b1e49
Damien Swarts <222868791@mycput.ac.za>
2026-05-08 15:04:05 +0200

    Added the street, city, postalCode and province of the person

--- src/main/java/za/ac/mycput/studentregistrationsystemspringboot/domain/Address.java
+++ src/main/java/za/ac/mycput/studentregistrationsystemspringboot/domain/Address.java
@@ -1,4 +1,28 @@
 package za.ac.mycput.studentregistrationsystemspringboot.domain;
 
+/*
+Address.java
+Address entity class representing a student's or lecturer's physical address
+
+Author: Damien Swarts (222868791)
+Date: 08 May 2026
+*/
+
 public class Address {
+    private String street;
+    private String city;
+    private String postalCode;
+    private String province;
+
+    public Address(String street, String city, String postalCode, String province) {
+        this.street = street;
+        this.city = city;
+        this.postalCode = postalCode;
+        this.province = province;
+    }
+
+    @Override
+    public String toString() {
+        return street + ", " + city + ", " + postalCode + ", " + province;
+    }
 }
