25993bd27b0f0887951c24e778b35014cd43ec57
Damien Swarts <222868791@mycput.ac.za>
2026-05-08 16:24:07 +0200

    I added the operations that is supose to happen in the interface class now I need to implement them in the Student Service class

--- src/main/java/za/ac/mycput/studentregistrationsystemspringboot/service/IStudentService.java
+++ src/main/java/za/ac/mycput/studentregistrationsystemspringboot/service/IStudentService.java
@@ -1,4 +1,24 @@
 package za.ac.mycput.studentregistrationsystemspringboot.service;
 
+import za.ac.mycput.studentregistrationsystemspringboot.domain.Student;
+
+import java.util.List;
+
+/*
+IStudentRepository.java
+Interface defining CRUD operations for Student.
+
+Author: Damien Swarts (222868791)
+Date: 08 May 2026
+*/
 public interface IStudentService {
+    Student create(Student student);
+
+    Student read(String id);
+
+    List<Student> getAll();
+
+    Student update(Student student);
+
+    void delete(String id);
 }
