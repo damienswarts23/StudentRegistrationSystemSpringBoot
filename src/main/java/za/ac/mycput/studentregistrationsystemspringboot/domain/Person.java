dc5287edf5e31c9e6379e923d0660ada2262eaa5
Damien Swarts <222868791@mycput.ac.za>
2026-05-08 17:06:17 +0200

    Had to make the Person class MappedSuperClass because it is being used by the Student class and added the embedded tag to every other class that gets used in this class. Then I also hadd to add a default constructor to the super class which means the Student should also add a default constructor since it implements the person class

--- src/main/java/za/ac/mycput/studentregistrationsystemspringboot/domain/Person.java
+++ src/main/java/za/ac/mycput/studentregistrationsystemspringboot/domain/Person.java
@@ -1,5 +1,8 @@
 package za.ac.mycput.studentregistrationsystemspringboot.domain;
 
+import jakarta.persistence.Embedded;
+import jakarta.persistence.MappedSuperclass;
+
 /*
 Person.java
 Abstract base class representing a generic person.
@@ -8,11 +11,15 @@ Provides common attributes shared by Student and Lecturer entities.
 Author: Damien Swarts (222868791)
 Date: 08 May 2026
 */
+@MappedSuperclass
 public abstract class Person {
     protected Address address;
     protected ContactDetails contactDetails;
     protected Gender gender;
     protected Race race;
+
+    protected Person() {
+    }
 
     public Person(Address address, ContactDetails contactDetails, Gender gender, Race race) {
         this.address = address;
@@ -21,18 +28,22 @@ public abstract class Person {
         this.race = race;
     }
 
+    @Embedded
     public Address getAddress() {
         return address;
     }
 
+    @Embedded
     public ContactDetails getContactDetails() {
         return contactDetails;
     }
 
+    @Embedded
     public Gender getGender() {
         return gender;
     }
 
+    @Embedded
     public Race getRace() {
         return race;
     }
