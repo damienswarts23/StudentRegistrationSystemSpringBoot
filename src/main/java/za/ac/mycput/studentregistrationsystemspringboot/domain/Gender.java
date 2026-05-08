12747c99260f0934498f0f69fc1f8091111c69ec
Damien Swarts <222868791@mycput.ac.za>
2026-05-08 15:09:27 +0200

    added the gender options for the enum class so when the student or lecturer can choose their gender

--- src/main/java/za/ac/mycput/studentregistrationsystemspringboot/domain/Gender.java
+++ src/main/java/za/ac/mycput/studentregistrationsystemspringboot/domain/Gender.java
@@ -1,4 +1,14 @@
 package za.ac.mycput.studentregistrationsystemspringboot.domain;
 
+/*
+Gender.java
+Enumeration representing gender categories for a person.
+
+Author: Damien Swarts (222868791)
+Date: 08 May 2026
+*/
 public enum Gender {
+    MALE,
+    FEMALE,
+    OTHER
 }
