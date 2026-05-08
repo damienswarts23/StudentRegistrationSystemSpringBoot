d9cddb5716b333ea5d4d58d7c2e2b35171890b1d
Damien Swarts <222868791@mycput.ac.za>
2026-05-08 15:16:55 +0200

    added all the classes to the Person class so when the student extends the person they get all of the attrabutes

--- src/main/java/za/ac/mycput/studentregistrationsystemspringboot/domain/Person.java
+++ src/main/java/za/ac/mycput/studentregistrationsystemspringboot/domain/Person.java
@@ -1,4 +1,39 @@
 package za.ac.mycput.studentregistrationsystemspringboot.domain;
 
+/*
+Person.java
+Abstract base class representing a generic person.
+Provides common attributes shared by Student and Lecturer entities.
+
+Author: Damien Swarts (222868791)
+Date: 08 May 2026
+*/
 public abstract class Person {
+    protected Address address;
+    protected ContactDetails contactDetails;
+    protected Gender gender;
+    protected Race race;
+
+    public Person(Address address, ContactDetails contactDetails, Gender gender, Race race) {
+        this.address = address;
+        this.contactDetails = contactDetails;
+        this.gender = gender;
+        this.race = race;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public ContactDetails getContactDetails() {
+        return contactDetails;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public Race getRace() {
+        return race;
+    }
 }
