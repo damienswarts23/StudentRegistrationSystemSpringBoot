551f2dd63565aa38357335e6b5bee5a5fd23fdbf
Damien Swarts <222868791@mycput.ac.za>
2026-05-08 16:15:51 +0200

    I created a interface student repository to help store data in the database (mysql)

--- src/main/java/za/ac/mycput/studentregistrationsystemspringboot/repository/StudentRepository.java
+++ src/main/java/za/ac/mycput/studentregistrationsystemspringboot/repository/StudentRepository.java
@@ -1,4 +1,14 @@
 package za.ac.mycput.studentregistrationsystemspringboot.repository;
 
-public interface StudentRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import za.ac.mycput.studentregistrationsystemspringboot.domain.Student;
+
+/*
+IStudentRepository.java
+Interface refering to the mysql database.
+
+Author: Damien Swarts (222868791)
+Date: 08 May 2026
+*/
+public interface StudentRepository extends JpaRepository<Student, String> {
 }
