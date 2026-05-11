0af7144179e9b9eab5ba5200bae60fa5168af1d3
Damien Swarts <222868791@mycput.ac.za>
2026-05-11 13:39:12 +0200

    extending the jpa repository which has everything we need and i just entered the class and its identifier datatype

--- src/main/java/za/ac/mycput/studentregistrationsystemspringboot/repository/DepartmentRepository.java
+++ src/main/java/za/ac/mycput/studentregistrationsystemspringboot/repository/DepartmentRepository.java
@@ -1,4 +1,14 @@
 package za.ac.mycput.studentregistrationsystemspringboot.repository;
 
-public interface DepartmentRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import za.ac.mycput.studentregistrationsystemspringboot.domain.Department;
+
+/*
+DepartmentRepository.java
+Interface refering to the mysql database.
+
+Author: Damien Swarts (222868791)
+Date: 11 May 2026
+*/
+public interface DepartmentRepository extends JpaRepository<Department, String> {
 }
