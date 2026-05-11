30d7c69cdc6970a0fb0b57bdac141fb7ec7b250a
Damien Swarts <222868791@mycput.ac.za>
2026-05-11 13:31:59 +0200

    Now there can be a department created with their id and name

--- src/main/java/za/ac/mycput/studentregistrationsystemspringboot/factory/DepartmentFactory.java
+++ src/main/java/za/ac/mycput/studentregistrationsystemspringboot/factory/DepartmentFactory.java
@@ -1,4 +1,21 @@
 package za.ac.mycput.studentregistrationsystemspringboot.factory;
 
+import za.ac.mycput.studentregistrationsystemspringboot.domain.Department;
+
+/*
+DepartmentFactory.java
+Factory class used to create Department objects.
+Encapsulates object creation logic for Department entities.
+
+Author: Damien Swarts (222868791)
+Date: 11 May 2026
+*/
+
 public class DepartmentFactory {
+    public static Department createDepartment(String departmentID,String departmentName){
+        return new Department.Builder()
+                .setDepartmentID(departmentID)
+                .setDepartmentName(departmentName)
+                .build();
+    }
 }
